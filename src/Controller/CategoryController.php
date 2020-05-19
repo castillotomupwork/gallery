8a2e954bd72f58e93f2186b863d3f113466fcb81
Tom Castillo <tomas.castillo@alternativemedia.com.au>
2020-05-20 03:10:04 +0800

    Code clean up

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,9 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
