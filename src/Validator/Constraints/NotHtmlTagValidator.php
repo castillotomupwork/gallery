fbb8ad014961fda278f7680399bf0568a38a5fc3
Tom Castillo <tomas.castillo@alternativemedia.com.au>
2020-05-20 03:12:32 +0800

    Code clean up

--- src/Validator/Constraints/NotHtmlTagValidator.php
+++ src/Validator/Constraints/NotHtmlTagValidator.php
@@ -9,7 +9,6 @@ class NotHtmlTagValidator extends ConstraintValidator
 {
     public function validate($protocol, Constraint $constraint)
     {
-//        if ($value != strip_tags($value)) {
         $combined_str = $protocol->getCategoryName().$protocol->getCategoryLink();
         if ($combined_str == 'TestLink') {
             $this->context->buildViolation($constraint->message)
